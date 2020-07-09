1628085a2c012651c7a20ceb04d73b6665483d09
Rainer Simon <rainer.simon@ait.ac.at>
2020-07-09 15:39:55 +0200

    Added tag vocabulary example

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ export class Recogito {
         onAnnotationDeleted={this.handleAnnotationDeleted}>
 
         <CommentWidget />
-        <TagWidget />
+        <TagWidget vocabulary={config.tagVocabulary} />
 
       </TextAnnotator>,
     
